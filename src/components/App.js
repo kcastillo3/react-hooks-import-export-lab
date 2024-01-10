301fd7a4330b23de657408df6de440f1b9d62cbe
Kevin <kevincastillo333@gmail.com>
2024-01-10 04:19:28 -0500

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import About from './About';
+import NavBar from './NavBar';
+import Home from './Home';
+import Details from '../data/user';
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={Details.username} city={Details.city}/>
+      <About image={Details.image}/>
     </div>
   );
 }
--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,15 @@
+
 const username = "Liza";
 const city = "New York";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
+
+const Details = {
+username,
+city,
+image
+};
+
+export default Details
